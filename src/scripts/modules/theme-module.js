f625bd4676f8e5f5884327ff22a007b2c3fa7aec
monica-cervantes <46661858+monica-cervantes@users.noreply.github.com>
2023-04-21 06:01:18 +0800

    new changes

--- src/scripts/modules/theme-module.js
+++ src/scripts/modules/theme-module.js
@@ -37,17 +37,65 @@ AppName.Modules.ThemeModule = (function() {
       var scrolled = $window.scrollTop(),
           win_height_padded = $window.height() * 1.1;
   
+      // $(".revealOnScroll.up:not(.animated)").each(function () {
+      //   var $this = $(this),
+      //       offsetTop = $this.offset().top;
+  
+      //   if (scrolled + win_height_padded > offsetTop) {
+      //     if ($this.data('timeout')) {
+      //       window.setTimeout(function(){
+      //         $this.addClass('animated fadeInUp');
+      //       }, parseInt($this.data('timeout'),10));
+      //     } else {
+      //       $this.addClass('animated fadeInUp');
+      //     }
+      //   }
+      // });
+
+      // $(".revealOnScroll.left:not(.animated)").each(function () {
+      //   var $this = $(this),
+      //       offsetTop = $this.offset().top;
+  
+      //   if (scrolled + win_height_padded > offsetTop) {
+      //     if ($this.data('timeout')) {
+      //       window.setTimeout(function(){
+      //         $this.addClass('animated fadeInLeft');
+      //       }, parseInt($this.data('timeout'),10));
+      //     } else {
+      //       $this.addClass('animated fadeInLeft');
+      //     }
+      //   }
+      // });
+
       $(".revealOnScroll:not(.animated)").each(function () {
         var $this = $(this),
             offsetTop = $this.offset().top;
   
         if (scrolled + win_height_padded > offsetTop) {
-          if ($this.data('timeout')) {
-            window.setTimeout(function(){
+          if($this.hasClass("up")) {
+            if ($this.data('timeout')) {
+              window.setTimeout(function(){
+                $this.addClass('animated fadeInUp');
+              }, parseInt($this.data('timeout'),10));
+            } else {
               $this.addClass('animated fadeInUp');
-            }, parseInt($this.data('timeout'),10));
-          } else {
-            $this.addClass('animated fadeInUp');
+            }
+          } else if ($this.hasClass("left")) {
+            if ($this.data('timeout')) {
+              window.setTimeout(function(){
+                $this.addClass('animated fadeInLeft');
+              }, parseInt($this.data('timeout'),10));
+            } else {
+              $this.addClass('animated fadeInLeft');
+            }
+          } else if ($this.hasClass("right")) {
+            if ($this.data('timeout')) {
+              window.setTimeout(function(){
+                $this.addClass('animated fadeInRight');
+              }, parseInt($this.data('timeout'),10));
+            } else {
+              $this.addClass('animated fadeInRight');
+            }
           }
         }
       });
